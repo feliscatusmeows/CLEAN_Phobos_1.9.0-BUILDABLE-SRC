db1b90203bac05ad3d5c95ef203e545d25a07796
feliscatusmeows <ericxinzemiao@gmail.com>
2021-05-16 18:44:49 -0700

    yote the mixin that breaks inertia compatibility

--- src/main/java/me/earth/phobos/mixin/mixins/MixinNettyCompressionDecoder.java
+++ src/main/java/me/earth/phobos/mixin/mixins/MixinNettyCompressionDecoder.java
@@ -8,12 +8,12 @@ import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(value={NettyCompressionDecoder.class})
 public abstract class MixinNettyCompressionDecoder {
-    @ModifyConstant(method={"decode"}, constant={@Constant(intValue=0x200000)})
-    private int decodeHook(int n) {
-        if (Bypass.getInstance().isOn() && Bypass.getInstance().packets.getValue().booleanValue() && Bypass.getInstance().noLimit.getValue().booleanValue()) {
-            return Integer.MAX_VALUE;
-        }
-        return n;
-    }
+//    @ModifyConstant(method={"decode"}, constant={@Constant(intValue=0x200000)})
+//    private int decodeHook(int n) {
+//        if (Bypass.getInstance().isOn() && Bypass.getInstance().packets.getValue().booleanValue() && Bypass.getInstance().noLimit.getValue().booleanValue()) {
+//            return Integer.MAX_VALUE;
+//        }
+//        return n;
+//    }
 }
 
